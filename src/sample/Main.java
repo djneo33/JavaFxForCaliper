e456d7e47092b388a444af90b038af27914740af
djlappy <djneo33@yahoo.com>
2019-12-28 22:39:29 -0600

    screen size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1080, 980));
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
 
 
